fe4cc2d4e3de06c7e6593a10e4b01d3b1c5a99d6
qlan3 <qlan3@ualberta.ca>
2023-02-13 15:44:17 -0700

    add requirement

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   python_requires='>=3.5',
   install_requires=[
     'numpy>=1.16.4',
-    'MinAtar>=1.0.4',
+    'MinAtar>=1.0.10',
     'gym>=0.13.0',
     'setuptools>=41.0.1',
     'pygame>=1.9.6',
